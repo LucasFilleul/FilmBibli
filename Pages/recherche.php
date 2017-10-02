0c4569978b7260323dd8f6d2fa7361a40a67083c
Lucas Filleul <filleul@info21-05.iut45.univ-orleans.fr>
2017-10-02 17:38:11 +0200

    Test recherche pas bon

---
+++ Pages/recherche.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <title> Films </title>
+</head>
+<body>
+  <?php
+  if($_SERVER['REQUEST_METHOD'] == "GET"){
+    echo "<form action='recherche.php'><br>";
+    echo "<br><h4>Quel est le type de votre recherche ?</h4>";
+    echo "<input type='radio' name='P_recherche' value='acteur'>Par Acteur";
+    echo "<input type='radio' name='P_recherche' value='genre'>Par Genre";
+    echo "<input type='radio' name='P_recherche' value='film'>Par Film";
+    echo "<br><br><input type='submit' value='Valider'></form>";
+  }
+  else{
+    if($_GET['P_recherche'] == "acteur"){
+      echo "ACTEUR FDP";
+    }
+    elseif($_GET['P_recherche'] == "genre"){
+      echo "GENRE FDP";
+    }
+    elseif($_GET['P_recherche'] == "film"){
+      echo "FILM FDP";
+    }
+  }
+  phpinfo(INFO_VARIABLES);
+  ?>
+</body>
+</html>
