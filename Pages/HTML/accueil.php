a6f1d512fb2af185eec43c40d275f8a12c396f46
Arthur Fauvin <fauvin@info12-26.iut45.univ-orleans.fr>
2017-10-04 14:10:13 +0200

    ajout dossier images + css

--- Pages/HTML/accueil.php
+++ Pages/HTML/accueil.php
@@ -3,8 +3,11 @@
 <head>
   <meta charset="utf-8" />
   <link rel="stylesheet" href="../CSS/accueil.css" />
-  <title> Films </title>
+  <title> Filleul and Co </title>
 </head>
+<header>
+  <img id ='header' src = '../images/bobine.jpg' style = 'width:50%'>
+</header>
 <body>
    <form action='fonctionnalités.php'><br>
    <br><h4>Voulez-vous accèder aux fonctionnalitées ?</h4>
