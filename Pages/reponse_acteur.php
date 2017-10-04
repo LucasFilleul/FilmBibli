c9834a4d0c176afb79f3bb37471e10fb13722917
Arthur Fauvin <fauvin@info24-03.iut45.univ-orleans.fr>
2017-10-04 13:28:51 +0200

    Recherche par genre et par acteurs anisi que par films sont disponible

--- Pages/reponse_acteur.php
+++ Pages/reponse_acteur.php
@@ -7,11 +7,29 @@
 </head>
 <body>
   <?php
+    // RESSORT L ID DU GENRE
     $file_db = new PDO("sqlite:../../BD/base_de_donnes_FILM.sqlite");
     $nom_recherche = $_GET['nom_recherche'];
     $request = $file_db->query("SELECT * FROM acteurs WHERE nom='$nom_recherche'");
     $donnees = $request->fetch();
-    echo "($donnees[0], $donnees[1], $donnees[2], $donnees[3], $donnees[4], $donnees[5])<br>";
+    $idacteur = $donnees[0];
+    if($idacteur == ""){
+      echo "L'acteur rentré n'est pas dans notre base de données.<br><br>";
+    }
+    else
+    {
+      // RESSORT LA LISTE DES ID DES FILMS DU GENRE.
+      $request_liste_id_acteurs = $file_db->query("SELECT ref_code_film FROM ACTEURDANSFILM WHERE ref_code_acteur='$idacteur'");
+      // RESSORT LE FILM
+      foreach ($request_liste_id_acteurs as $idfilm)
+      {
+      $request_films = $file_db->query("SELECT * FROM films WHERE code_film ='$idfilm[0]'");
+        foreach ($request_films as $film)
+        {
+          print $film[0] . ", " . $film[1] . ", " .$film[2] . ", " .$film[3] . ", " .$film[4] . ", " .$film[5] . ", " .$film[6] . ", " .$film[7] . ", " . $film[8] . "<br>";
+        }
+      }
+    }
     echo "Voulez vous retourner à l'accueil ?";
     echo "<form action='accueil.php'><br>";
     echo "<input type='submit' value='Accueil'></form>";
--- Pages/reponse_genre.php
+++ Pages/reponse_genre.php
@@ -13,17 +13,26 @@
     $request = $file_db->query("SELECT * FROM genres WHERE nom_genre='$nom_recherche'");
     $donnees = $request->fetch();
     $idgenre = $donnees[0];
-
-    // RESSORT LA LISTE DES FILMS DU GENRE
-    $file_db2 = new PDO("sqlite:../../BD/base_de_donnes_FILM.sqlite");
-    $request_liste_films = $file_db2->query("SELECT ref_code_film FROM FILMESTDEGENRE WHERE ref_code_genre='$idgenre'");
-    foreach ($request_liste_films as $film) {
-     print $film[0] . "<br>";
+    if($idgenre == ""){
+      echo "Le genre rentré n'est pas dans notre base de données.<br><br>";
     }
-
-    //echo "Voulez vous retourner à l'accueil ?";
-    // echo "<form action='accueil.php'><br>";
-    // echo "<input type='submit' value='Accueil'></form>";
+    else
+    {
+      // RESSORT LA LISTE DES ID DES FILMS DU GENRE.
+      $request_liste_films = $file_db->query("SELECT ref_code_film FROM FILMESTDEGENRE WHERE ref_code_genre='$idgenre'");
+      // RESSORT LE FILM
+      foreach ($request_liste_films as $idfilm)
+      {
+      $request_films = $file_db->query("SELECT * FROM films WHERE code_film ='$idfilm[0]'");
+        foreach ($request_films as $film)
+        {
+          print $film[0] . ", " . $film[1] . ", " .$film[2] . ", " .$film[3] . ", " .$film[4] . ", " .$film[5] . ", " .$film[6] . ", " .$film[7] . ", " . $film[8] . "<br>";
+        }
+      }
+    }
+    echo "Voulez vous retourner à l'accueil ?";
+    echo "<form action='accueil.php'><br>";
+    echo "<input type='submit' value='Accueil'></form>";
 
   ?>
 </body>
