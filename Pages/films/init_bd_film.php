4cd1d1996f6a93aed323c4167a3b04088f18c7ca
Lucas Filleul <filleul@info22-15.iut45.univ-orleans.fr>
2017-10-10 12:58:52 +0200

    modif bd film

--- Pages/films/init_bd_film.php
+++ Pages/films/init_bd_film.php
@@ -22,7 +22,7 @@
     )");
     $films = array(
       // COMEDIE
-      array(1,'Qu\'est ce qu\'on a fait au bon dieu','FR', 2014, 137,'Couleur','Philippe de Chauveron','aubondieu.jpg'),
+      array(1,'Qu est ce qu on a fait au bon dieu','FR', 2014, 137,'Couleur','Philippe de Chauveron','aubondieu.jpg'),
       array(2,'The Mask','USA', 1994, 141,'Couleur','Chuck Russell' ,'mask.jpg'),
       array(3,'Cops : Les Forces du désordre','USA', 2015, 144,'Couleur','Luke Greenfield' ,'cops.jpg'),
       array(4,'Mais qui a tué Pamela Rose ?','FR', 2003, 135,'Couleur','Éric Lartigau' ,'pamelarose.jpg'),
@@ -33,13 +33,13 @@
       array(9,'Les Huit Salopards','USA', 2015, 317,'Couleur', 'Quentin Tarantino','salopars.jpg'),
       array(10,'Kill Bill: Volume 1','USA', 2003, 152,'Couleur', 'Quentin Tarantino','killbill.jpg'),
       array(11,'2012','USA', 2009, 238,'Couleur', 'Roland Emmerich','2012.jpg'),
-      array(12,'Il était une fois dans l\'Ouest','USA/ITA', 1968, 255,'Couleur', ' ergio Leone','ouest.jpg'),
+      array(12,'Il était une fois dans l Ouest','USA/ITA', 1968, 255,'Couleur', ' ergio Leone','ouest.jpg'),
       array(13,'Le Masque de Zorro','USA', 1998, 216, 'Couleur', 'Martin Campbell','zorro.jpg'),
-      array(14,'Omar m\'a tuer','FR', 2011, 125, 'Couleur', 'Roschdy Zem','omar.jpg'),
+      array(14,'Omar m a tuer','FR', 2011, 125, 'Couleur', 'Roschdy Zem','omar.jpg'),
       array(15,'Un havre de paix','USA', 2013, 156, 'Couleur', 'Lasse Hallström','havrepaix.jpg'),
       array(16,'Indigènes','FR', 2006, 208, 'Couleur', 'Rachid Bouchareb','indigene.jpg'),
       array(17,'Percy Jackson : Le Voleur de foudre','USA', 2010, 158, 'Couleur', 'Chris Columbus','percy.jpg'),
-      array(18,'Les Aventuriers de l\'arche perdue','USA', 1981, 155, 'Couleur', 'Steven Spielberg','indi.jpg'),
+      array(18,'Les Aventuriers de l arche perdue','USA', 1981, 155, 'Couleur', 'Steven Spielberg','indi.jpg'),
       array(19,'Gone Girl','USA', 2014, 229,'Couleur', 'David Fincher','gonegirl.jpg'),
       array(20,'Avatar','USA', 2009, 242,'Couleur', 'James Cameron','avatar.jpg'),
       array(21,'Star Wars, épisode IV : Un nouvel espoir','USA', 1977, 205,'Couleur', 'George Lucas','starwars.jpg'),
