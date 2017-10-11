acc1dc648d85d54243dacfd9a299366a8cd3b2ff
Lucas Filleul <filleul@info24-10.iut45.univ-orleans.fr>
2017-10-11 18:45:21 +0200

    Ajouter Film

--- Pages/films/ajouter_film.php
+++ Pages/films/ajouter_film.php
@@ -26,26 +26,23 @@
       $file_db = new PDO('sqlite:../../../BD/base_de_donnes_FILM.sqlite');
       $requete_code = $file_db->query("SELECT max(code_film) FROM films");
       $donnees = $requete_code->fetch();
-      $insert = "INSERT INTO films (code_film,titre,pays,date, duree,couleur, realisateur, image)
-      VALUES (:code_film,:titre,:pays,:date, :duree,:couleur, :realisateur, :image)";
-      if($_POST["titre"] == "" || $_POST["pays"] == ""
-      || $_POST["date"] == "" || $_POST["duree"] == "" || $_POST["couleur"] == ""
-      || $_POST["realisateur"] == ""){
-        echo "<fieldset id='blanc'>";
-        echo "<h4 id='blanc'>Le film n'a pas été ajouté !<br> Les informations doivent etre éronées.";
-        echo "<form action='../films/ajouter_film.php'><br>";
-        echo "<input type='submit' value='Retour'></form>";
-        echo "</fieldset>";
-      }
-      else{
+      $insert = "INSERT INTO films (code_film,titre,pays,date, duree,couleur, image)
+      VALUES (:code_film,:titre,:pays,:date, :duree,:couleur, :image)";
+      // if($_POST["titre"] == "" || $_POST["pays"] == "" || $_POST["date"] == "" || $_POST["duree"] == "" || $_POST["couleur"] == "" || $_POST["realisateur"] == ""){
+      //   echo "<fieldset id='blanc'>";
+      //   echo "<h4 id='blanc'>Le film n'a pas été ajouté !<br> Les informations doivent etre éronées.";
+      //   echo "<form action='../films/ajouter_film.php'><br>";
+      //   echo "<input type='submit' value='Retour'></form>";
+      //   echo "</fieldset>";
+      // }
+      // else{
         $stmt = $file_db->prepare($insert);
-        $stmt->bindValue(':code_film', $donnees[0] + 1);
+        $stmt->bindValue(':code_film',$donnees[0] + 1 );
         $stmt->bindParam(':titre', $_POST["titre"]);
         $stmt->bindParam(':pays', $_POST["pays"]);
         $stmt->bindParam(':date', $_POST["date"]);
         $stmt->bindParam(':duree', $_POST["duree"]);
         $stmt->bindParam(':couleur', $_POST["couleur"]);
-        $stmt->bindParam(':realisateur', $_POST["realisateur"]);
         $stmt->bindValue(':image', "NB.jpg");
         $stmt->execute();
         echo "<fieldset id='blanc'>";
@@ -53,7 +50,7 @@
         echo "<form action='liste_films.php'><br>";
         echo "<input type='submit' value='Retour'></form>";
         echo "</fieldset>";
-      }
+      // }
       $file_db = null;
     }
     if($_SERVER['REQUEST_METHOD'] == "GET"){
@@ -65,7 +62,6 @@
       echo "Date de sortie : <input type='text' name='date' required placeholder='2001'><br><br>";
       echo "Durée du film  : <input type='text' name='duree' required placeholder='155 (1h55min)'><br><br>";
       echo "Couleur        : <input type='text' name='couleur' required placeholder='NB (Noir et Blanc) / Couleur'><br><br>";
-      echo "Réalisateur    : <input type='text' name='realisateur' required placeholder='Stanley Kubrick'><br><br>";
       echo "<input type='submit' value='Valider'></form>";
       echo "</fieldset>";
     }
